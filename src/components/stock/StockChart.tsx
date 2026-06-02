1b1d92cafc0eb65b79a91808a1cb485869b9259a
jooho2075 <parkjh2075@icloud.com>
2026-06-02 12:48:09 +0900

    bug(assets)#70: 증권 계좌 조회 시 X-Firebase-Uid 헤더 누락 수정

--- src/components/stock/StockChart.tsx
+++ src/components/stock/StockChart.tsx
@@ -17,8 +17,6 @@ export default function StockChart({ data, height = 200 }: StockChartProps) {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const chart = createChart(containerRef.current, {
-
-    const chartOptions: any = {
       width: containerRef.current.clientWidth,
       height,
       attributionLogo: false,
@@ -41,9 +39,6 @@ export default function StockChart({ data, height = 200 }: StockChartProps) {
         timeVisible: true,
       },
     } as any);
-    };
-
-    const chart = createChart(containerRef.current, chartOptions);
 
     const candleSeries = chart.addSeries(CandlestickSeries, {
       upColor: '#22c55e',
