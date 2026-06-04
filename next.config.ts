c0c22f13056af37ae74b0725aefef466a4f6befa
ktx06155 <ktx06155@naver.com>
2026-06-04 11:59:58 +0900

    chore(infra): .dockerignore 수정(.env.local 제외)

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,26 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/v1/ai/:path*",
-                destination: "http://service-ai-server:8000/api/v1/ai/:path*",
-            },
-            {
-                source: "/api/:path*",
-                destination: "http://service-backend:8080/api/:path*",
-            },
-            {
-                source: "/mydata/:path*",
-                destination: "http://mydata-server:8084/mydata/:path*",
-            },
-            {
-                source: "/baas/:path*",
-                destination: "http://transaction-server:8083/baas/:path*",
-            },
-        ];
-    },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/ai/:path*",
+        destination: "http://service-ai-server:8000/api/v1/ai/:path*",
+      },
+      {
+        source: "/api/:path*",
+        destination: "http://service-backend:8080/api/:path*",
+      },
+      {
+        source: "/mydata/:path*",
+        destination: "http://mydata-server:8084/mydata/:path*",
+      },
+      {
+        source: "/baas/:path*",
+        destination: "http://transaction-server:8083/baas/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
