06f261129ccd452b332d9e6aabbc76b13360df15
hyun7931 <119102748+hyun7931@users.noreply.github.com>
2026-06-07 23:13:51 +0900

    feat(infra): config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,7 @@ const nextConfig: NextConfig = {
             },
             {
                 source: "/baas/:path*",
-                destination: `${process.env.TRANSACTION_URL ?? "http://10.10.4.107:8083"}/baas/:path*`,
+                destination: `${process.env.TRANSACTION_URL ?? "http://transaction-server:8083"}/baas/:path*`
             },
         ];
     },
